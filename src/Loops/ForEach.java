4864bcb00ab86b754bbfe29ccfae430ff78ea522
Mainak0290 <mainkskdr@gmail.com>
2026-01-29 00:18:02 +0530

    Adding ForEach.java class

---
+++ src/Loops/ForEach.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] arr = {"Sun", "Mon", "Tue", "Wed", "Thurs", "Fri"};
+        for(String i:arr)
+            System.out.print(i+" ");
+    }
+}

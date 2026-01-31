313ce9f1b67fa20ca211442c5822e06140309dde
Mainak0290 <mainkskdr@gmail.com>
2026-01-31 16:56:05 +0530

    Adding SwitchCaseWithNumber.java and SwitchCaseWithCharacter.java.java

---
+++ src/SwitchCase/SwitchCaseWithCharacter.java
@@ -0,0 +1,23 @@
+package SwitchCase;
+
+public class SwitchCaseWithCharacter {
+
+    public static void main(String[] args) {
+    char ch ='E';
+    switch(ch) {
+        case 'B':
+            System.out.println('B');
+            break;
+        case 'C':
+            System.out.println('C');
+            break;
+        case 'A':
+            System.out.println('A');
+            break;
+        default:
+            System.out.println("Test");
+
+    }
+
+    }
+}
---
+++ src/SwitchCase/SwitchCaseWithNumber.java
@@ -0,0 +1,22 @@
+package SwitchCase;
+
+public class SwitchCaseWithNumber {
+    public static void main(String[] args) {
+        int N=10;
+        switch(N) {
+            case 11:
+                System.out.println("11");
+                break;
+
+            case 12:
+                System.out.println("12");
+                break;
+
+            case 10:
+                System.out.println("10");
+                break;
+            default:
+                System.out.println("Other");
+        }
+    }
+}
